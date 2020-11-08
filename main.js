7728007a01a99cbd3f3f864117929e1294b4caef
qqweng <qqweng@ucdavis.edu>
2020-11-07 21:56:10 -0700

    updating some info

---
+++ main.js
@@ -0,0 +1,11 @@
+'use strict';
+
+const e = React.createElement;
+
+class HomePage extends React.Component {
+    render() {
+    }
+}
+
+const domContainer = document.querySelector('#myBio');
+ReactDOM.render(domContainer)
